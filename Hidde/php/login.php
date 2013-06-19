247bf0ed393d82bc8468ac68cc75b82e39e97139
hiddehensel <hiddehensel@gmail.com>
2013-06-19 11:01:20 +0200

    Connection to server

---
+++ Hidde/php/login.php
@@ -0,0 +1,38 @@
+<?php
+
+	#Setup variables
+	$firstname = $_POST["FirstNameToSearch"];
+
+	#Avoid SQL injection attacks
+	//$firstname = mysql_real_escape_string($firstname);
+
+	#Connect to Database
+	$con = mysql_connect("mysql11-int.cp.hostnet.nl","u30984_fhd","stayconnected", "db30984_stayc");
+	if (!$con)
+	{
+		die('Could not connect: ' . mysql_error());
+	}
+
+	#Select the test database
+	mysql_select_db("db30984_stayc", $con);
+
+	#Get the user details from the database
+	$userdetails = mysql_query("INSERT INTO testdatabase (user, location) VALUES ('$firstname', 'testlocatie')");
+
+	#Catch any errors
+	if (!$userdetails) {
+		echo 'Could not run query: ' . mysql_error();
+		exit;
+	}
+
+
+	#Get the first row of the results
+	//$row = mysql_fetch_row($userdetails);
+
+	#Build the result array (Assign keys to the values)
+	$result_data = "Success";
+
+	#Output the JSON data
+	echo $result_data; 
+
+?>
